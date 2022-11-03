65ebe49d6f0fdebea8f449f422c7ac2c56c39b0e
vaish0207 <vaishu0299@gmail.com>
2022-11-03 13:45:50 -0400

    route53-record

--- csye6225-deleteStack.sh
+++ csye6225-deleteStack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Deleting the stack with name $1"
 
-aws cloudformation describe-stacks --stack-name $1 --region $2 >/dev/null 2>&1 --profile demo
+aws cloudformation describe-stacks --stack-name $1 --region $2 >/dev/null 2>&1 --profile $3
 if [ $? -ne 0 ]
 then
 	echo "Failed: Stack with  name $1 doesnt exist"
@@ -9,12 +9,12 @@ then
 fi
 
 status=$(aws cloudformation delete-stack \
---stack-name $1 --region $2 --profile demo)
+--stack-name $1 --region $2 --profile $3)
 
 if [ $? -eq 0 ]
 then
     echo "Waiting on $1 for delete completion..."
-    aws cloudformation wait stack-delete-complete --stack-name $1 --region $2 --profile demo
+    aws cloudformation wait stack-delete-complete --stack-name $1 --region $2 --profile $3
     if [ $? -eq 0 ]
     then
         echo "Stack has been successfully Deleted"
